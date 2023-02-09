1b049122bc2c1efe17aeaec3c0fd8574707050e5
Mateusz Gawroński <gawronski.m91@gmail.com>
2023-02-09 13:40:23 +0100

    add test empty_list

--- factors.py
+++ factors.py
@@ -1,9 +1,12 @@
 
 
-number = 3958159171
+number = 3958159172
+prime_digits = []
+
 
 def prime_factors(number):
     while number % 2 == 0:
+        prime_digits.append(2)
         number /= 2
 
     
--- test_factors.py
+++ test_factors.py
@@ -1,4 +1,4 @@
-from factors import prime_factors, number
+from factors import prime_factors, number, prime_digits
 
 
 def test_import_prime_factors():
@@ -18,13 +18,17 @@ def test_number_divided_by_2():
     assert number % 2 == 0, "number is odd"
 
 
+def test_list_is_empty():
+    assert len(prime_digits) == 0, "list is not empty"  
+
+
 
 
 
 
 if __name__ == '__main__':
     for test in (
-        test_import_prime_factors, test_variable_is_integer, test_number_divided_by_2,
+        test_import_prime_factors, test_variable_is_integer, test_number_divided_by_2, test_list_is_empty,
     ):
         print(f'{test.__name__}: ', end=' ')
         try:
