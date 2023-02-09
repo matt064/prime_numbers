628226f7fbf35197d1536d67e9b7137967effb80
Mateusz Gawroński <gawronski.m91@gmail.com>
2023-02-09 15:24:58 +0100

    add test primary_digits

--- factors.py
+++ factors.py
@@ -1,14 +1,25 @@
 
 
-number = 3958159172
-prime_digits = []
-
-
 def prime_factors(number):
-    while number % 2 == 0:
-        prime_digits.append(2)
-        number /= 2
+    prime_digits = []
 
+    if not isinstance(number, int):
+        raise ValueError("Number must be an integer")
+    
+    if number / number == 1 and number / 1 == number and number % 2 != 0 and number % 3 != 0:
+        prime_digits.append(number)
+    else:
+
+        while number % 2 == 0:
+            prime_digits.append(2)
+            number /= 2
+
+    
+
+
+
+
+    return prime_digits
     
 
 
--- test_factors.py
+++ test_factors.py
@@ -1,4 +1,4 @@
-from factors import prime_factors, number, prime_digits
+from factors import prime_factors
 
 
 def test_import_prime_factors():
@@ -10,25 +10,32 @@ def test_import_prime_factors():
 
 
 def test_variable_is_integer():
-    if not isinstance(number, int):
-        raise ValueError('variable is not an integer')
+    try:
+        prime_factors('tata')
+        assert False, "ValueError expected"
+    except ValueError:
+        pass
 
 
-def test_number_divided_by_2():
-    assert number % 2 == 0, "number is odd"
+# def test_number_divided_by_2():
+#     assert number % 2 == 0, "number is odd"
 
 
-def test_list_is_empty():
-    assert len(prime_digits) == 0, "list is not empty"  
+def test_list_is_not_empty():
+    lista = prime_factors(12)
+    assert len(lista) != 0, "list is empty"  
 
 
+def test_primary_digit():
+    primary = prime_factors(11)
+    assert primary == [11], "liczba nie jest liczba pierwsza"
 
 
 
 
 if __name__ == '__main__':
     for test in (
-        test_import_prime_factors, test_variable_is_integer, test_number_divided_by_2, test_list_is_empty,
+        test_import_prime_factors, test_variable_is_integer, test_list_is_not_empty, test_primary_digit
     ):
         print(f'{test.__name__}: ', end=' ')
         try:
